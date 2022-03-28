fac8e989849441b5564cc66eb592d14ca0018a2e
LetoCode <pavolov.egor@gmail.com>
2022-03-28 16:43:13 +0300

    upd2

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Topic(models.Model):
     """Тема, которую изучает пользователь"""
     text = models.CharField(max_length=200)
@@ -8,3 +9,20 @@ class Topic(models.Model):
     def __str__(self):
         """Возвращает строковое представление модели"""
         return self.text
+
+
+class Entry(models.Model):
+    """Информаци, изученная пользователем по теме"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
